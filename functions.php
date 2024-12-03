c7f5d6114ba24c940eb5430f9ff2eaf49674123c
D-Bars <awotik26@gmail.com>
2024-12-03 16:44:33 +0100

    created basic structure

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+
+add_action('wp_enqueue_scripts', function(){
+    wp_enqueue_style('main-css',get_template_directory_uri() . '/assets/css/main.css');
+    wp_enqueue_style( 'font-Montserrat', get_template_directory_uri() . '/assets/fonts', array(), false, true);
+
+    wp_enqueue_script('main-js',   get_template_directory_uri() . '/assets/js/main.js',array(), false, true);
+    wp_enqueue_script('jquery' );
+});
+
+
+function get__localize__title($objId) {
+    $obj_title = get_the_title($objId);
+    return printf(__('%s', 'firm'), $obj_title);
+}
+function get__localize__content($objId) {
+    $obj_content = get_the_content($objId);
+    return printf(__('%s', 'firm'), $obj_content);
+}
+
+function wp_dump( $data ) {
+	echo "<pre>" . print_r( $data, 1 ) . "</pre>";
+}
