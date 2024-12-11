ff831cb4d22d6f86be08c526b3bc9ec58748db9e
D-Bars <awotik26@gmail.com>
2024-12-12 00:06:14 +0100

    added advantages structure basic(static 1)

--- front-page.php
+++ front-page.php
@@ -1,5 +1,34 @@
 <?php get_header();  ?>
 
-
+<div class="advantages__block">
+    <div class="advantages__item a">
+        <div class="advantages__lamp__wrapper"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/lamp.png" alt="AdvantagesLamp"></div>
+        <div class="advantages__item__content__box">
+            <div class="advantages__item__title">1</div>
+            <div class="advantages__item__subtitle">2</div>
+        </div>
+    </div>
+    <div class="advantages__item">
+        <div class="advantages__lamp__wrapper"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/lamp.png" alt="AdvantagesLamp"></div>
+        <div class="advantages__item__content__box">
+            <div class="advantages__item__title">1</div>
+            <div class="advantages__item__subtitle">2</div>
+        </div>
+    </div>
+    <div class="advantages__item">
+        <div class="advantages__lamp__wrapper"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/lamp.png" alt="AdvantagesLamp"></div>
+        <div class="advantages__item__content__box">
+            <div class="advantages__item__title">1</div>
+            <div class="advantages__item__subtitle">2</div>
+        </div>
+    </div>
+    <div class="advantages__item">
+        <div class="advantages__lamp__wrapper"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/lamp.png" alt="AdvantagesLamp"></div>
+        <div class="advantages__item__content__box">
+            <div class="advantages__item__title">1</div>
+            <div class="advantages__item__subtitle">2</div>
+        </div>
+    </div>
+</div>
 
 <?php get_footer();  ?>
