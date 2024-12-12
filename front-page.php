8081bd8f967d5400d81f0906c84b12c5bd1d873a
D-Bars <awotik26@gmail.com>
2024-12-13 00:39:58 +0100

    reworked logic advantages animation lamp

--- front-page.php
+++ front-page.php
@@ -1,8 +1,15 @@
 <?php get_header();  ?>
 
 <div class="advantages__block">
-    <div class="advantages__item a">
-        <div class="advantages__lamp__wrapper"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/lamp.png" alt="AdvantagesLamp"></div>
+    <div class="advantages__item active">
+        <div class="advantages__lamp__wrapper">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/lamp.png" alt="AdvantagesLamp">
+            <div class="rays__box">
+                <div class="ray ray__first"></div>
+                <div class="ray ray__second"></div>
+                <div class="ray ray__third"></div>
+            </div>
+        </div>
         <div class="advantages__item__content__box">
             <div class="advantages__item__title">1</div>
             <div class="advantages__item__subtitle">2</div>
