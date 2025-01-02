9860ae62bb0000caf53ca5121cb73b2013e8d938
D-Bars <awotik26@gmail.com>
2025-01-03 00:54:30 +0100

    fixed: btn(WP Forms) :focus; added: custom footer dates

--- footer.php
+++ footer.php
@@ -1,6 +1,39 @@
 <footer>
-    <div class="block1"></div>
-    <div class="block2"></div>
+    <?php
+    global $theme__options;
+    ?>
+    <div class="footer__contact__block">
+        <div class="footer__contact__item">
+            <i class="fa-solid fa-phone footer__contact__ico"></i>
+            <?php if($theme__options['footer__phone']): ?>
+                <a class="footer__contact__name" href="tel:+<?php echo str_replace(array('+', '-', ' '), array('', '', ''), $theme__options['footer__phone']) ?>">
+                    <?php echo $theme__options['footer__phone']; ?>
+                </a>
+            <?php endif; ?>
+        </div>
+        <div class="footer__contact__item">
+            <i class="fa-regular fa-envelope footer__contact__ico"></i>
+            <?php if($theme__options['footer__mail']): ?>
+                <a class="footer__contact__name" href="mailto:<?php echo $theme__options['footer__mail']; ?>">
+                    <?php echo $theme__options['footer__mail']; ?>
+                </a>
+            <?php endif; ?>
+        </div>
+        <div class="footer__contact__item">
+            <i class="fa-solid fa-file footer__contact__ico"></i>
+            <?php if($theme__options['footer__nip']): ?>
+                <div class="footer__contact__name"><?php echo $theme__options['footer__nip']; ?></div>
+            <?php endif; ?>
+        </div>
+    </div>
+    <div class="footer__design__el"></div>
+    <div class="footer__logo__wrapper">
+        <?php
+        if ($theme__options['footer__logo']):
+        echo '<img src="' . esc_url($theme__options['footer__logo']) . '" alt="FooterLogo">'
+        ?>
+        <?php endif; ?>
+    </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
--- incs/customizer.php
+++ incs/customizer.php
@@ -8,7 +8,6 @@ add_action('customize_register', function (WP_Customize_Manager $wp_customize){
         'priority' => 10
     ));
 
-    //Header title
     $wp_customize->add_setting('header__title');
     $wp_customize->add_control('header__title', array(
         'label' => __('Заголовок шапки', 'firm'),
@@ -16,7 +15,6 @@ add_action('customize_register', function (WP_Customize_Manager $wp_customize){
         'type' => 'textarea'
     ));
 
-    //Header description
     $wp_customize->add_setting('header__description');
     $wp_customize->add_control('header__description', array(
         'label' => __('Описание/подзаголовок шапки', 'firm'),
@@ -24,7 +22,6 @@ add_action('customize_register', function (WP_Customize_Manager $wp_customize){
         'type' => 'textarea'
     ));
 
-    //Header custom image
     $wp_customize->add_setting('header__image', array(
         'default'           => '',
         'sanitize_callback' => 'esc_url_raw'
@@ -34,7 +31,6 @@ add_action('customize_register', function (WP_Customize_Manager $wp_customize){
         'section'  => 'header__center__options',
     )));
 
-    //Header mask
     $wp_customize->add_setting('header__mask_enable', array(
         'default' => false,
         'sanitize_callback' => 'wp_validate_boolean',
@@ -53,7 +49,6 @@ add_action('customize_register', function (WP_Customize_Manager $wp_customize){
         'priority' => 10
     ));
 
-    //About us text
     $wp_customize->add_setting('about__us__text');
     $wp_customize->add_control('about__us__text', array(
         'label' => __('Текст о нас', 'firm'),
@@ -61,7 +56,6 @@ add_action('customize_register', function (WP_Customize_Manager $wp_customize){
         'type' => 'textarea'
     ));
 
-    //About us image
     $wp_customize->add_setting('about__us__image', array(
         'default'           => '',
         'sanitize_callback' => 'esc_url_raw'
@@ -70,19 +64,57 @@ add_action('customize_register', function (WP_Customize_Manager $wp_customize){
         'label'    => __('Изображение о нас', 'firm'),
         'section'  => 'about__us__options',
     )));
+
+    //Section footer
+    $wp_customize->add_section('footer__options', array(
+        'title' => __('Footer', 'firm'),
+        'priority' => 10
+    ));
+
+    $wp_customize->add_setting('footer__phone');
+    $wp_customize->add_control('footer__phone', array(
+        'label' => __('Footer номер телефона', 'firm'),
+        'section' => 'footer__options',
+    ));
+
+    $wp_customize->add_setting('footer__mail');
+    $wp_customize->add_control('footer__mail', array(
+        'label' => __('Footer емейл', 'firm'),
+        'section' => 'footer__options',
+    ));
+
+    $wp_customize->add_setting('footer__nip');
+    $wp_customize->add_control('footer__nip', array(
+        'label' => __('Footer НИП', 'firm'),
+        'section' => 'footer__options',
+    ));
+
+    $wp_customize->add_setting('footer__logo', array(
+        'default'           => '',
+        'sanitize_callback' => 'esc_url_raw'
+    ));
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'footer__logo', array(
+        'label'    => __('Изображение в footer', 'firm'),
+        'section'  => 'footer__options',
+    )));
 });
 
 function phoenix__theme__options(){
     global $theme__options;
     $theme__options = array(
-        //header options
+
         'header__title' => get_theme_mod('header__title'),
         'header__description' => get_theme_mod('header__description'),
         'header__image' => get_theme_mod('header__image'),
         'header__mask' => get_theme_mod('header__mask_enable'),
-        //about us options
+
         'about__us__text' => get_theme_mod('about__us__text'),
-        'about__us__image' => get_theme_mod('about__us__image')
+        'about__us__image' => get_theme_mod('about__us__image'),
+
+        'footer__phone' => get_theme_mod('footer__phone'),
+        'footer__mail' => get_theme_mod('footer__mail'),
+        'footer__nip' => get_theme_mod('footer__nip'),
+        'footer__logo' => get_theme_mod('footer__logo')
     );
 }
 add_action('wp', 'phoenix__theme__options');
