7d5f4cfbe24a1f2d8fdb71b3d8e0cd56ec00c33f
D-Bars <awotik26@gmail.com>
2025-01-03 14:15:09 +0100

    changed: structure HTML(footer__contact__item)

--- footer.php
+++ footer.php
@@ -5,24 +5,34 @@
     <div class="footer__contact__block">
         <div class="footer__contact__item">
             <i class="fa-solid fa-phone footer__contact__ico"></i>
-            <?php if($theme__options['footer__phone']): ?>
-                <a class="footer__contact__name" href="tel:+<?php echo str_replace(array('+', '-', ' '), array('', '', ''), $theme__options['footer__phone']) ?>">
-                    <?php echo $theme__options['footer__phone']; ?>
-                </a>
+            <?php if ($theme__options['footer__phone']): ?>
+                <div class="footer__contact__item__box">
+                    <div class="footer__contact__title"><?php _e('Телефон', 'firm'); ?></div>
+                    <a class="footer__contact__name"
+                        href="tel:+<?php echo str_replace(array('+', '-', ' '), array('', '', ''), $theme__options['footer__phone']) ?>">
+                        <?php echo $theme__options['footer__phone']; ?>
+                    </a>
+                </div>
             <?php endif; ?>
         </div>
         <div class="footer__contact__item">
             <i class="fa-regular fa-envelope footer__contact__ico"></i>
-            <?php if($theme__options['footer__mail']): ?>
-                <a class="footer__contact__name" href="mailto:<?php echo $theme__options['footer__mail']; ?>">
-                    <?php echo $theme__options['footer__mail']; ?>
-                </a>
+            <?php if ($theme__options['footer__mail']): ?>
+                <div class="footer__contact__item__box">
+                    <div class="footer__contact__title"><?php _e('Почта', 'firm'); ?></div>
+                    <a class="footer__contact__name" href="mailto:<?php echo $theme__options['footer__mail']; ?>">
+                        <?php echo $theme__options['footer__mail']; ?>
+                    </a>
+                </div>
             <?php endif; ?>
         </div>
         <div class="footer__contact__item">
             <i class="fa-solid fa-file footer__contact__ico"></i>
-            <?php if($theme__options['footer__nip']): ?>
-                <div class="footer__contact__name"><?php echo $theme__options['footer__nip']; ?></div>
+            <?php if ($theme__options['footer__nip']): ?>
+                <div class="footer__contact__item__box">
+                    <div class="footer__contact__title"><?php _e('НИП', 'firm'); ?></div>
+                    <div class="footer__contact__name"><?php echo $theme__options['footer__nip']; ?></div>
+                </div>
             <?php endif; ?>
         </div>
     </div>
@@ -30,8 +40,8 @@
     <div class="footer__logo__wrapper">
         <?php
         if ($theme__options['footer__logo']):
-        echo '<img src="' . esc_url($theme__options['footer__logo']) . '" alt="FooterLogo">'
-        ?>
+            echo '<img src="' . esc_url($theme__options['footer__logo']) . '" alt="FooterLogo">'
+                ?>
         <?php endif; ?>
     </div>
 </footer>
